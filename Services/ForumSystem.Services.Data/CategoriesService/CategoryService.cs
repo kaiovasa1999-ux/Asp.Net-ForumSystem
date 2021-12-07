915d4757b97a528f1ce632ee031f31e7685c7f04
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2021-12-07 17:07:06 +0200

    Added new Service for CategoryController

---
+++ Services/ForumSystem.Services.Data/CategoriesService/CategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace ForumSystem.Services.Data.CategoriesService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class CategoryService
+    {
+    }
+}
---
+++ Services/ForumSystem.Services.Data/CategoriesService/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using ForumSystem.Web.ViewModels.Home;
+
+namespace ForumSystem.Services.Data.CategoriesService
+{
+    public interface ICategoryService
+    {
+        CategriesviewModel FindByTitle(string title);
+    }
+}
